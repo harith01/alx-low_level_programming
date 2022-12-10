af6941bceb8e4ecf04422bb9384b5146c5840806
harith01 <harithyusuph@gmail.com>
2022-12-10 22:01:04 +0000

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Descriptio:
+ * Returns: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpa);
+	}
+	return (0);
+}
