10f53cf263f5ccc04a3c260ac6fa888d3a417292
harith01 <harithyusuph@gmail.com>
2022-12-11 12:27:25 +0000

    taks-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
