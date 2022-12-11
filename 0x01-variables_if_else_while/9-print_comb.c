948ca8f1f1e999a2699fdd39d9500294477a54e1
harith01 <harithyusuph@gmail.com>
2022-12-11 15:23:07 +0000

    task-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
