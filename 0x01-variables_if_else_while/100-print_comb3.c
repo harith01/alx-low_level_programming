0f694780fb3894dd61d5cae98ba48093cef06597
harith01 <harithyusuph@gmail.com>
2022-12-11 16:06:48 +0000

    task-100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit1;
+	int digit2;
+
+	while (digit1 <= '8')
+	{
+		for (digit2 = digit1 + 1; digit2 <= '9'; digit2++)
+		{
+			putchar(digit1);
+			putchar(digit2);
+			if ((digit1 != '8') && (digit2 != '9'))
+			{
+				putchar(44);
+				putchar(32);
+			}
+			
+		}
+		digit1++;
+	}
+	return (0);
+}
