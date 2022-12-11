6a53b46ce833c4c7dcbe36b6d9d1a314af8626e9
harith01 <harithyusuph@gmail.com>
2022-12-11 13:10:46 +0000

    task-6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char digit;
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
