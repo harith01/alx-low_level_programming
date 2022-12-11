572f440b4d823c7d854879d27cf0679f6e465f0c
harith01 <harithyusuph@gmail.com>
2022-12-11 13:26:22 +0000

    task-8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		putchar(digit);
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
+
