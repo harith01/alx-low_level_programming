509c7f929f24ec6b4f11aa526f1895a82167fc96
harith01 <harithyusuph@gmail.com>
2022-12-13 14:45:05 +0000

    task-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - function
+ * Description: print a-z
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a', alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n);
+	return (0);
+}
