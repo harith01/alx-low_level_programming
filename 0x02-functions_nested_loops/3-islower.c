349e6a597c67ee2597edc6a475b61a1b31f59f65
harith01 <harithyusuph@gmail.com>
2022-12-13 15:52:55 +0000

    taks-3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - check if alphabet is lower case
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
