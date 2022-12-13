ff74b1f6f45e1375a151e3498a1d914f59a77f9d
harith01 <harithyusuph@gmail.com>
2022-12-13 14:46:28 +0000

    task-1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,10 @@ void print_alphabet(void)
 {
 	int alphabet;
 
-	for (alphabet = 'a', alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
 	}
-	_putchar('\n);
+	_putchar('\n');
 	return (0);
 }
