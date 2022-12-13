1b0a5fa374e1cb98df4bacd43ae3d36362195cbd
harith01 <harithyusuph@gmail.com>
2022-12-13 18:04:44 +0000

    task-8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of JB
+ * 
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour_1 = '0';
+	int hour_2 = '0';
+	int min_1 = '0';
+	int min_2 = '0';
+
+	while (hour_1 < '3')
+	{
+		while (hour_2 < '4')
+		{
+			while (min_1 < '6')
+			{
+				while (min_2 <= '9')
+				{
+					_putchar(hour_1);
+					_putchar(hour_2);
+					_putchar(58);
+					_putchar(min_1);
+					_putchar(min_2);
+					min_2++;
+				}
+				min_1++;
+			}
+			hour_2++;
+		}
+		hour_1++;
+	}
+}
