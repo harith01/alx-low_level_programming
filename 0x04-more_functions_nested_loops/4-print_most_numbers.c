8afecd8b2cb648217eeba8648b09bb9dc96d60e7
harith01 <harithyusuph@gmail.com>
2022-12-15 19:37:51 +0000

    task-4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9
+ *
+ * Return: 01356789
+ */
+
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if (i == 50 || i == 52)
+			break
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
