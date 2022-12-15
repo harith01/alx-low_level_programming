4fbddabd44949a9e4f18ccccb55f643ce4fdbf2a
harith01 <harithyusuph@gmail.com>
2022-12-15 19:26:38 +0000

    task-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ *
+ * Return: 0123456789
+ */
+
+void print_numbers(void)
+{
+	int i = '0';
+
+	while (i < '10')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
