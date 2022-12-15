5d99c44f14716860bc169f9b569b5782153435ad
harith01 <harithyusuph@gmail.com>
2022-12-15 21:35:53 +0000

    task-5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - 0 to 14, 10 times
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void);
+{
+	int i = 0;
+	int num;
+
+	while (i < 10)
+	{
+		num = 0;
+		while (num < 15)
+		{
+			if (num > 9)
+			{
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
+			}
+			else
+			{
+				_putchar(num);
+			}
+		}
+	}
+}
