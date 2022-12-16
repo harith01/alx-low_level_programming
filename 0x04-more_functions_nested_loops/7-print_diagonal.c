2cd90e2e2a6c4df1a93c0636310397e383d8761c
harith01 <harithyusuph@gmail.com>
2022-12-16 13:08:43 +0000

    task-7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal lines
+ * @n: number of lines
+ * Return: Always 0
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+	else
+		if (n == 1)
+		{
+			_putchar(92);
+		}
+		else
+		{
+			for (i = 0; i < n; i++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+		}
+}
