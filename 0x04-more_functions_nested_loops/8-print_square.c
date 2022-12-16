4599b2d94f1028a6373e78d71016f7b51df76a24
harith01 <harithyusuph@gmail.com>
2022-12-16 13:43:44 +0000

    task-8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - print # to form a square
+ * @size: size of the square
+ * Return: Nothing
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
