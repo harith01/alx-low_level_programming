f290812b82af94d96ce993291f7b0617eba03a40
harith01 <harithyusuph@gmail.com>
2022-12-19 14:29:13 +0000

    task-0-remove whitespace

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 /**
- * void_reset_to_98 - reset value to 98
+ * reset_to_98 - reset value to 98
  * @n: variable to reset
  * Return: Nothing
  */
 void reset_to_98(int *n)
 {
-	*n = 98;	
+	*n = 98;
 }
