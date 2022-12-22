a8750db692ba63ba4a5fe3dcf400466a4ad5e98b
harith01 <harithyusuph@gmail.com>
2022-12-22 20:38:01 +0000

    task-3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: difference between the ascii chars
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, o;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+		{
+			o = 0;
+			break;
+		}
+		else
+		{
+			o = s1[i] - s2[i];
+			break;
+		}
+	}
+	return (o);
+}
