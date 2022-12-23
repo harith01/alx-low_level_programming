af2153df2655aff5c383e6fa758f609f93c535df
harith01 <harithyusuph@gmail.com>
2022-12-23 14:46:28 +0000

    task-5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,7 +10,7 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
@@ -18,5 +18,5 @@ char *string_toupper(char *str)
 		}
 		i++;
 	}
-	return (*str);
+	return (str);
 }
