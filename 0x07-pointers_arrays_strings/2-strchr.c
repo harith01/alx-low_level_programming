0b4ada5a52f5d9386cdc743536f442826f15d4a7
harith01 <harithyusuph@gmail.com>
2023-01-02 16:50:49 +0000

    task-2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (*s == c)
+		if (s[i] == c)
+		{
 			return (s);
-		*s++;
+		}
+		i++;
 	}
+	return ('/0');
 }
