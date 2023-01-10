318b95ae955cf2544b9aa42bf3a5f26bfbd6e7ef
harith01 <harithyusuph@gmail.com>
2023-01-10 11:02:32 +0000

    task-4-free memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			for (i--; i > 0; i--)
+			for (i--; i >= 0; i--)
 				free(grid[i]);
 			free(grid);
 		}
