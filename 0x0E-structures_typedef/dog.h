57d0fc13211aef4215889d2f2c75d69f521bfbe7
harith01 <harithyusuph@gmail.com>
2023-01-16 13:11:49 +0000

    task-0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Keeps record of dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
