61d950479d70ae406a8d33e49dc40c658eee63e2
harith01 <harithyusuph@gmail.com>
2023-01-17 08:19:56 +0000

    task-1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,16 +7,16 @@
  * @name: name
  * @age: age
  * @owner: owner
- * Return: Nothingi
+ * Return: Nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return (NULL);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-
-	return (d);
+	d = malloc(sizeof(struct dog));
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
