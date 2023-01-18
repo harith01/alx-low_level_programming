aa7989523c0029956168216107c4340d7e7a2b94
harith01 <harithyusuph@gmail.com>
2023-01-18 13:20:35 +0000

    task-1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -18,5 +18,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 	for (i = 0; i < size; i++)
-		action(*(array + i));
+		action(array[i]);
 }
