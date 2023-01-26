7d45d5fee5bfc92358d81d642f4ab89aefef0b67
harith01 <harithyusuph@gmail.com>
2023-01-26 22:02:27 +0000

    task-100

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,4 +22,6 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void first(void) __attribute__ ((constructor));
+
 #endif
