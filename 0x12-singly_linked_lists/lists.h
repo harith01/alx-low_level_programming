53f208c0f0166ef9dffcd93844c2c13c6478282e
harith01 <harithyusuph@gmail.com>
2023-01-26 13:52:08 +0000

    Create header file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+#include <stddef.h>
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+
+
+#endif
