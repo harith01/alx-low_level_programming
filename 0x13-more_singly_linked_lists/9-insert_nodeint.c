c0bf3e68ccddbd17be62261a2b0f1053e78ed0b5
harith01 <harithyusuph@gmail.com>
2023-01-31 13:57:36 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *add_prev_node, *new_node, *ptr;
 
+	if (*head == NULL && idx != 0)
+		return (NULL);
+
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
