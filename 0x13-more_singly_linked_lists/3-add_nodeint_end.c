7015907b8bb2e3ffbd34e8a3e1d60817c20ff429
harith01 <harithyusuph@gmail.com>
2023-01-31 13:22:54 +0000

    Remove trailing whitespace

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = n;
 	new_node->next = NULL;
 
