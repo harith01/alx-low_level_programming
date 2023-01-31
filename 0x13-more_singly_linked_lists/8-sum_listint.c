43905c1d5aeefc8bbc1868f048b3afb004549199
harith01 <harithyusuph@gmail.com>
2023-01-31 13:13:58 +0000

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_listint - Add the data in a linked list
+ * @head: pointer to the head of the list
+ * Return: sum of the data
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
