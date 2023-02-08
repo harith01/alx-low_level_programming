50a06aa2705973ae6efbc3359c0ed6f738bd67cd
harith01 <harithyusuph@gmail.com>
2023-02-08 14:33:03 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[len])
-			leni++;
+			len++;
 		w = write(fd, text_content, len);
 		if (w != len)
 			return (-1);
