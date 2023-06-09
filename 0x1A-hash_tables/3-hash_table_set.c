a922caa840c6416938d1a53d557e5b67c74ab7de
harith01 <harithyusuph@gmail.com>
2023-06-09 14:38:09 +0000

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,41 +10,46 @@
  * @value: the value
  * Return: 1 if succeeded, 0 otherwise
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+
+ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *current_node;
-	unsigned long int index;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	
+
 	value_copy = strdup(value);
-	if (!value_copy)
+	if (value_copy == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
 
-	current_node = ht->array[index];
-	node = malloc(sizeof(hash_node_t);
-	if (!node)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
 		return (0);
-	if (current_node == NULL)
-	{
-		ht->array[index] = node;
-		node->value = strdup(value);
-		node->next = NULL;
-		return (1);
 	}
-	if (strcmp(current_node->key, key) == 0)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		ht->array[index]->value = strdup(value);
-		free(node);
-		return (1);
+		free(new);
+		return (0);
 	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
-	node->value = strdup(value);
-	node->key = strdup(key);
-	node->next = ht->array[index];
-	ht->array[index] = node;
 	return (1);
 }
