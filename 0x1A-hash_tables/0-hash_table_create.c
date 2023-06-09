fe663d6d16d479d1a6ad2492f293b12d6eaa64b0
harith01 <harithyusuph@gmail.com>
2023-06-09 08:27:22 +0000

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,11 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (table == NULL)
 		return (NULL);
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
-	if (!table->array)
+	if (table->array == NULL)
 		return (NULL);
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
